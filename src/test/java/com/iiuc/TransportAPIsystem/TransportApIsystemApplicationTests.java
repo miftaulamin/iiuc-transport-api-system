7e42e0cf477a85cb02587a7bb9b45f4aa0dde7bf
CODEWITH-SB <bhowmicksusmoy067@gmail.com>
2026-05-15 22:04:29 +0600

    spring boot add

---
+++ src/test/java/com/iiuc/TransportAPIsystem/TransportApIsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiuc.TransportAPIsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportApIsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
