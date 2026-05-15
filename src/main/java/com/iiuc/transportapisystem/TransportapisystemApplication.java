574050869ef46720a73cef8f60ec80bdfec3e20d
absorris-del <absorris@gmail.com>
2026-05-15 22:03:46 +0600

    comment

---
+++ src/main/java/com/iiuc/transportapisystem/TransportapisystemApplication.java
@@ -0,0 +1,13 @@
+package com.iiuc.transportapisystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportapisystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportapisystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iiuc/transportapisystem/TransportapisystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiuc.transportapisystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportapisystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
