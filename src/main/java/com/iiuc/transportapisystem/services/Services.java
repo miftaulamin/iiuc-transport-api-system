b0fbf8f4bbf699c1de39f01a6f6a3d8067b88684
miftaulamin <khannirob385@gmail.com>
2026-06-04 19:42:20 +0600

    Model Layer Done

---
+++ src/main/java/com/iiuc/transportapisystem/services/Services.java
@@ -0,0 +1,10 @@
+package com.iiuc.transportapisystem.services;
+
+import com.iiuc.transportapisystem.model.Model;
+import com.springframework.stereotype.Service;
+import java.util.*;
+
+@service
+public class Services{
+
+}
